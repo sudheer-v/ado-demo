90a6476457be40eee492f15b77c36e0d48a4a49a
sudheer-v <sudheer_valla@yahoo.com>
2022-08-03 18:46:33 +0100

    Update sonarqube_add_user.sh

--- tools/sonarqube_add_user.sh
+++ tools/sonarqube_add_user.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 import sys
+from ast import literal_eval
+from os.path import exists
 from sonarqube import SonarQubeClient
 
-sonarcloud_url = "http://34.133.185.189:9000"
+sonarcloud_url = (sys.argv[3])
 sonarcloud_token = (sys.argv[1])
+sonarcloud_project = (sys.argv[2])
+
 sonar = SonarQubeClient(sonarqube_url=sonarcloud_url, token=sonarcloud_token)
 
-sonar.permissions.add_permission_to_user(login="user2", permission="admin", projectKey="abc")
+file_exists = exists('code_owners.txt')
+if file_exists:
+    file_data = open("code_owners.txt", "r")
+    data = file_data.read()
+    file_data.close()
+    users = set(literal_eval(data))
+    for user in users:
+        print(f"""granting permissions to + {user} + on Sonarqube Project""")
+        sonar.permissions.add_permission_to_user(login=user, permission="admin", projectKey=sonarcloud_project)
